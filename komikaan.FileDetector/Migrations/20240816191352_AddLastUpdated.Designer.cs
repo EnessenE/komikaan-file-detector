548b1871bac0e793410af1997389489873ceb6f4
Enes <9263360+EnessenE@users.noreply.github.com>
2024-08-16 21:14:22 +0200

    LastAttempt

--- komikaan.FileDetector/Migrations/20240816191352_AddLastUpdated.Designer.cs
+++ komikaan.FileDetector/Migrations/20240816191352_AddLastUpdated.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using komikaan.FileDetector.Contexts;
@@ -11,9 +12,11 @@ using komikaan.FileDetector.Contexts;
 namespace komikaan.FileDetector.Migrations
 {
     [DbContext(typeof(SupplierContext))]
-    partial class SupplierContextModelSnapshot : ModelSnapshot
+    [Migration("20240816191352_AddLastUpdated")]
+    partial class AddLastUpdated
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ komikaan.FileDetector/Migrations/20240816191352_AddLastUpdated.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace komikaan.FileDetector.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddLastUpdated : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "last_attempt",
+                table: "supplier_configurations",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTimeOffset(new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 0, 0, 0, 0)));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "last_attempt",
+                table: "supplier_configurations");
+        }
+    }
+}
