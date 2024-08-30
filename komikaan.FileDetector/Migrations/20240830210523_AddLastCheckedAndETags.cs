b4981026225ec130f075f38e134385572654a10b
Enes <9263360+EnessenE@users.noreply.github.com>
2024-08-30 23:07:28 +0200

    Updates

---
+++ komikaan.FileDetector/Migrations/20240830210523_AddLastCheckedAndETags.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace komikaan.FileDetector.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddLastCheckedAndETags : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "e_tag",
+                table: "supplier_configurations",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "last_checked",
+                table: "supplier_configurations",
+                type: "timestamp with time zone",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "e_tag",
+                table: "supplier_configurations");
+
+            migrationBuilder.DropColumn(
+                name: "last_checked",
+                table: "supplier_configurations");
+        }
+    }
+}
